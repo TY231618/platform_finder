c52bd92dfbc79eebaf7e78cab44475133f09b864
Chris Wynne <chris.wynndow@gmail.com>
2016-02-03 18:31:08 +0000

    Adds check and wait for platform number as well as change of Twilio auth token and hidden .env file

--- app/worker/api_caller.rb
+++ app/worker/api_caller.rb
@@ -5,12 +5,17 @@ class ApiCaller
   include Sidekiq::Worker
 
   def perform(mobile, time, start_station, end_station)
-    response = HTTParty.get('https://huxley.apphb.com/departures/' + start_station  + '/to/' + end_station + '/5?accessToken=e841e6e4-acfb-40bf-8879-53a69322adaa')
-
-    selected_train =  response.to_hash['trainServices'].select{ |train| train['std'] == time}
-
-    if selected_train['platform'] != nil
-      Text.get_text(mobile, selected_train['platform'])
+    complete = false
+    until complete == true
+      response = HTTParty.get('https://huxley.apphb.com/departures/' + start_station  + '/to/' + end_station + '/5?accessToken=e841e6e4-acfb-40bf-8879-53a69322adaa')
+      selected_train = response.to_hash['trainServices'].select{ |train| train['std'] == time}
+      if selected_train.first['platform'] != nil
+        Text.get_text(mobile, selected_train.first['platform'])
+        complete = true
+      else
+        sleep(10)
+        puts "Checking with API"
+      end
     end
   end
 end
