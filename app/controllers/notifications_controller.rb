7ce9376cfb0a215018f6591da822082d545f55df
Chris Wynne <chris.wynndow@gmail.com>
2016-02-03 20:00:45 +0000

    Adds a notification page and route

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,9 +2,13 @@ class NotificationsController < ApplicationController
 
   skip_before_action :verify_authenticity_token, only: [:create]
 
+  def index
+    
+  end
+
   def create
     ApiCaller.perform_async(params[:mobile], params[:time], params[:station], params[:destination])
-    redirect_to root_path
+    redirect_to notifications_path
   end
 
 end
