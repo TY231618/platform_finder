4c0d49560078d6f1823350d792c694eabb6116f1
Chris Wynne <chris.wynndow@gmail.com>
2016-02-04 12:14:45 +0000

    Adds test for ApiCaller and Text.rb

--- spec/text_spec.rb
+++ spec/text_spec.rb
@@ -7,6 +7,5 @@ describe Text do
     expect(Text.send_platform(mobile: "07843699606", platform:"5", working_number: "+15005550006", my_account_sid: ENV["TEST_TWILIO_ACCOUNT_SID"],
     my_auth_token: ENV["TEST_TWILIO_AUTH_TOKEN"]).class).to equal(Twilio::REST::Message)
 
-    # expect(Text.message).not_to eq nil
   end
 end
